99d567cfa83e92ca04e5f2837b934b3dd55ca7ec
dom <34490428+qu-ota@users.noreply.github.com>
2018-12-06 08:32:13 -0500

    Create server.cs

---
+++ server.cs
@@ -0,0 +1,5 @@
+//YAMA (Yet Another Moderator Addon)
+//By Dominoes (37977)
+//A better moderator addon, including moderator-supported module loading and playerlist updating
+
+//code coming soon, will add module loading and core function loading later
