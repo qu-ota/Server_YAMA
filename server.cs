6d3322f94a5b30724838c2fead780b84928fc421
dom <34490428+qu-ota@users.noreply.github.com>
2018-12-08 10:33:39 -0500

    woah code

--- server.cs
+++ server.cs
@@ -2,4 +2,58 @@
 //By Dominoes (37977)
 //A better moderator addon, including moderator-supported module loading and playerlist updating
 
-//code coming soon, will add module loading and core function loading later
+echo("YAMA | Loading main functions"); //Ahead Client Tools by Ahead, edited to load everything from YAMA instead of "Client_Ahead"
+function loadMainYAMAModules()
+{
+	if(isFile("Add-Ons/Server_YAMA/core.cs"))
+	{
+		echo("YAMA | Core found, loading and continuing");
+		exec("Add-Ons/Server_YAMA/core.cs");
+		$YAMA::coresLoaded = 1;
+	}
+	else
+	{
+		$YAMA::coresLoaded = 0;
+		return error("YAMA | Core file not found, not executing");
+	}
+}
+
+function loadYAMAModules() //function for finding & loading modules
+{
+	%dir = "Add-Ons/Server_YAMA/modules/*.cs";
+	%fileCount = getFileCount(%dir);
+	%filename = findFirstFile(%dir);
+	%dirCount = 0;
+	while(%filename !$= "")
+	{
+		%path = filePath(%filename);
+		%dirName = getSubStr(%path, strlen("Add-Ons/Server_YAMA/modules/"), strlen(%path) - strlen("Add-Ons/Server_YAMA/modules/"));
+		%dirNameList[%dirCount] = %dirName;
+		%dirCount = %dirCount + 1.0;
+		%filename = findNextFile(%dir);
+	}
+	%i = 0;
+	while(%i < %dirCount)
+	{
+		%dirName = %dirNameList[%i];
+		echo("YAMA | Checking module: " @ %dirName);
+		%name = %dirName;
+		echo("YAMA | Loading module: " @ %dirName);
+		exec("Add-Ons/Server_YAMA/modules/" @ %dirName);
+		%i = %i + 1.0;
+	}
+}
+echo("YAMA | Ensuring core files are present...");
+loadMainYAMAModules();
+echo("YAMA | Ensuring everything's good so far...");
+if($YAMA::coresLoaded)
+{
+	continue;
+}
+else
+{
+	return error("YAMA | Add-on not loaded, cancelling");
+}
+echo("YAMA | Everything looks good and loaded properly, now looking for modules");
+loadYAMAModules();
+echo("YAMA | Good to go!");
