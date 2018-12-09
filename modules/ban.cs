5bad2fd7b06580349c25fdcc73153f0125774b9c
dom <34490428+qu-ota@users.noreply.github.com>
2018-12-08 21:57:36 -0500

    revamped ban script

--- modules/ban.cs
+++ modules/ban.cs
@@ -1,61 +1,51 @@
 //Ban command for moderators
-//Originally created by Tezuni, plus a couple enhancements by Dominoes including reason input
+//Originally found in Crown's moderator, plus a couple enhancements by Dominoes
 
-function serverCmdB(%client,%player,%time,%c1,%c2,%c3,%c4,%c5,%c6,%c7,%c8,%c9,%c10,%c11,%c12,%c13,%c14,%c15,%c16,%c17,%c18,%c19,%c20)
+function serverCmdBan(%cl, %a, %target, %time, %reason) 
 {
-	%player = findClientByName(%player);
-	
-	if(%c1 !$= "")
+	if(%cl.isAdmin)
 	{
-		for(%a = 1; %a < 21; %a++)
+		if(%reason=="")
+			%reason = "You have been banned by server staff. | Issued by" SPC %client.name;
+		else
+			%reason =  %reason SPC "| Issued by" SPC %client.name;
+		
+		parent::serverCmdBan(%cl, %a, %target, %time, %reason);
+		return;
+	}
+	%victim = findClientByName(%target.bl_id);
+	if(%cl.isModerator) 
+	{
+		if(%victim.isAdmin || %victim.isModerator || hasItemOnList($Pref::Server::AutoAdminList, %target) || hasItemOnList($Pref::Server::AutoSuperAdminList, %target))
 		{
-			if(%c[%a] !$= "")
-			{
-				%player.reason = %player.reason SPC %c[%a] SPC "| Issued by" SPC %client.name;
-			}
-		}
-	}
-	else
-	{
-		%player.reason = "You were banned by server staff. | Issued by" SPC %client.name;
-	}
-	
-	if(%client.isAdmin)
-	{
-		if(%player.isAdmin || %player.isSuperAdmin)
-			return messageClient(%client, '', "\c2You cannot ban other administrators with this command.");
-
-        if(!isObject(%player))
-        	return messageClient(%client, '', "\c2Player \c3" @ %player.name @ "\c2 not found.");
-
-		if(%time=="")
-			%time=10;
-
-		serverCmdBan(%client,%player.getPlayerName(), %player.BL_ID, %time, %player.reason SPC "| Issued by" SPC %client.name);
-		return;	
-	}
-	
-	if(%client.isModerator)
-	{
-		if(%client.ModeratorCommandSpam + 5 > $Sim::Time)
+			messageClient(%cl,'',"\c6You may not ban other staff members.");
 			return;
+		}
 		
-		%client.ModeratorCommandSpam = $Sim::Time;
+		if(%time > $Pref::Server::Moderator::MaxBanLength || %time == -1 && $Pref::Server::Moderator::MaxBanLength != -1)
+		{
+			messageClient(%cl,'', "\c7You may only ban players for up to\c6" SPC $Pref::Server::Moderator::MaxBanLength SPC "\c7minutes.");
+			return;
+		}
 		
-		if(%player.isModerator || %player.isAdmin)
-			return messageClient(%client, '', "\c6You cannot ban other server staff with this command.");
-
-        if(!isObject(%player))
-        	return messageClient(%client, '', "\c6Player not found."); 
-
-        %client.isAdmin = 1;
-        messageAll('MsgAdminForce', '\c6  ==> [\c4MOD\c6] \c4%1 \c6banned \c4%2\c2(ID: %3)', %Client.name, %Player.name, %Player.bl_id);
+		if(%cl.getBLID() $= %target)
+		{
+			messageClient(%cl,'',"\c7Nice try, \c6" @ %client.name @ "\c7, but go seek attention somewhere else.");
+			return;
+		}
 		
-		%time=10;
-        serverCmdBan(%client,%player.getPlayerName(), %player.BL_ID, %time, %player.reason SPC "|Issued by" SPC %client.name);
-
-		%client.isAdmin = 0;
-		return;	
+		if(%reason=="")
+			%reason = "You have been banned by server staff. | Issued by" SPC %client.name;
+		else
+			%reason =  %reason SPC "| Issued by" SPC %client.name;
+		
+		serverCmdBan(%cl, %a, %target, %time, %reason);
+		return;
 	}
-	messageClient(%client, '', "\c3/K \c2is for server staff only (\c3Moderator+\c2).");
+	if(%reason=="")
+		%reason = "You have been banned by server staff. | Issued by" SPC %client.name;
+	else
+		%reason =  %reason SPC "| Issued by" SPC %client.name;
+	
+	parent::serverCmdBan(%cl, %a, %target, %time, %reason);
 }
