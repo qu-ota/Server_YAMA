686dcf318a2a2dc521929be7717deb57b5465caf
dom <34490428+qu-ota@users.noreply.github.com>
2018-12-09 11:02:55 -0500

    Create lists.cs

---
+++ lists.cs
@@ -0,0 +1,27 @@
+//Functions for adding and removing moderators from the dedicated list
+//Made by Crown
+
+function hasItemOnList(%string,%item) {
+	for(%i=0;%i<getWordCount(%string);%i++) {
+		%word = getWord(%string,%i);
+		if(%word $= %item)
+			return true;
+	}
+	return false;
+}
+
+function addItemToList(%string,%item) {
+	if(!hasItemOnList(%string, %item))
+		%string = %string SPC %item;
+	return %string;
+}
+
+function removeItemFromList(%string,%item) {
+	for(%i=0;%i<getWordCount(%string);%i++) {
+		%word = getWord(%string,%i);
+		if(%word $= %item)
+			continue;
+		%fString = %fString SPC %word;
+	}
+	return trim(%fString);
+}
