3eef1f239fe1f13be6b0d4f97dd8910f2615ad88
dom <34490428+qu-ota@users.noreply.github.com>
2018-12-09 11:01:35 -0500

    shorter script yay

--- modules/kick.cs
+++ modules/kick.cs
@@ -1,55 +1,40 @@
 //Kick command for moderators
-//Originally created by Tezuni, plus a couple enhancements by Dominoes including reason input
+//Originally created by Crown, plus a couple enhancements by Dominoes including reason input
 
-function servercmdK(%client,%Player,%c1,%c2,%c3,%c4,%c5,%c6,%c7,%c8,%c9,%c10,%c11,%c12,%c13,%c14,%c15,%c16,%c17,%c18,%c19,%c20)
+function serverCmdKick(%cl,%id,%c1,%c2,%c3,%c4,%c5,%c6,%c7,%c8,%c9,%c10,%c11,%c12,%c13,%c14,%c15,%c16,%c17,%c18,%c19,%c20) 
 {
-	%player = findClientByName(%player);
-	
 	if(%c1 !$= "")
 	{
 		for(%a = 1; %a < 21; %a++)
 		{
 			if(%c[%a] !$= "")
 			{
-				%player.reason = %player.reason SPC %c[%a] SPC "| Issued by" SPC %client.name;
+				%id.reason = %id.reason SPC %c[%a] SPC "| Issued by" SPC %client.name;
 			}
 		}
 	}
 	else
 	{
-		%player.reason = "You were kicked by server staff. | Issued by" SPC %client.name;
+		%id.reason = "You were kicked by server staff. | Issued by" SPC %client.name;
 	}
 	
-	if(%client.isAdmin)
+	if(%id $= "")
 	{
-		if(%player.isAdmin || %player.isSuperAdmin)
-			return messageClient(%client, '', "\c2You cannot kick other administrators with this command.");
-
-        if(!isObject(%player))
-        	return messageClient(%client, '', "\c2Player \c3" @ %player.name @ "\c2 not found.");
-        	
-        messageAll('MsgAdminForce', '\c2[\c3Admin\c2] \c3%1 \c2kicked \c3%2\c2(ID: %3)', %client.name, %player.name, %player.bl_id);
-	    %player.delete("%player.reason");
-		return;	
+		messageClient(%cl,'',"\c7Invalid syntax; probably a user that doesn't exist.");
+		messageClient(%cl,'',"\c7Proper syntax: \c6/kick \c3name \c420-word reason");
+		return;
 	}
-
-	if(%client.isModerator)
+	
+	if(%cl.isModerator) 
 	{
-		if(%client.ModeratorCommandSpam + 5 > $Sim::Time)
+		if(%id.isAdmin || %id.isModerator)
+		{
+			messageClient(%cl,'',"\c7You may not kick other staff members.");
 			return;
-		
-		%client.ModeratorCommandSpam = $Sim::Time;
-		
-		if(%player.isModerator || %player.isAdmin)
-			return messageClient(%client, '', "\c6You cannot kick other staff members with this command.");
-
-        if(!isObject(%player))
-        	return messageClient(%client, '', "\c2Player \c3" @ %player.name @ "\c2 not found.");
-
-        messageAll('MsgAdminForce', '\c6[\c4MOD\c6] \c4%1 \c6kicked \c4%2\c6(ID: %3)', %client.name, %player.name, %player.bl_id);
-		%player.delete("%player.reason");
-		return;	
+		}
+		messageAll('MsgAdminForce', '\c2[\c3Moderator\c2] \c3%1 \c2kicked \c3%2\c2(ID: %3)', %client.name, %player.name, %player.bl_id);
+		%id.delete(%id.reason);
+		return;
 	}
-	
-	messageClient(%client, '', "\c3/K \c2is for server staff only (\c3Moderator+\c2).");
+	parent::serverCmdKick(%cl,%id,%c1,%c2,%c3,%c4,%c5,%c6,%c7,%c8,%c9,%c10,%c11,%c12,%c13,%c14,%c15,%c16,%c17,%c18,%c19,%c20);
 }
